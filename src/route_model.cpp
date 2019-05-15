53004dd7c949dad2266224655cf53ce40b335550
Luke <hallmluke@gmail.com>
2019-05-15 20:25:48 +0000

    Initial commit

---
+++ src/route_model.cpp
@@ -0,0 +1,48 @@
+#include "route_model.h"
+#include <iostream>
+
+RouteModel::RouteModel(const std::vector<std::byte> &xml) : Model(xml) {
+    int i = 0;
+    for(Model::Node node : this->Nodes()) {
+        m_Nodes.push_back(Node(i, this, node));
+        i++;
+    }
+    CreateNodeToRoadHashmap();
+}
+
+void RouteModel::CreateNodeToRoadHashmap() {
+    for(auto road : Roads()) {
+        if(road.type != Model::Road::Type::Footway) {
+            for(auto node_idx : Ways()[road.way].nodes) {
+                if(node_to_road.find(node_idx) == node_to_road.end()){
+                    node_to_road[node_idx] = std::vector<const Model::Road*>{};
+                }
+                node_to_road[node_idx].push_back(road);
+            }
+        }
+    }
+}
+
+RouteModel::Node* RouteModel::Node::FindNeighbor(std::vector<int> node_indices){
+    RouteModel::Node* closest_node = nullptr;
+    RouteModel::Node node;
+    for(int node_index : node_indices) {
+        node = parent_model->SNodes()[node_index];
+        if(this->distance(node) > 0 && node.visited == false) {
+            if(closest_node == nullptr || this->distance(node) < this->distance(*closest_node)){
+                closest_node = &parent_model->SNodes()[node_index];
+            }
+        }
+    }
+
+    return closest_node;
+}
+
+void RouteModel::Node::FindNeighbors() {
+    for( auto road : parent_model->node_to_road[this->index]){
+        auto closestNeighbor = FindNeighbor(parent_model->Ways()[road->way].nodes);
+        if(closestNeighbor != nullptr) {
+            this->neighbors.push_back(closestNeighbor);
+        }
+    }
+}
