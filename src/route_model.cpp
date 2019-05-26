c80323de3756152b4066d85c296afcf50114a2c3
Luke Hall <hallmluke@gmail.com>
2019-05-26 14:07:45 -0400

    Fixed node to road hashmap

--- src/route_model.cpp
+++ src/route_model.cpp
@@ -11,13 +11,13 @@ RouteModel::RouteModel(const std::vector<std::byte> &xml) : Model(xml) {
 }
 
 void RouteModel::CreateNodeToRoadHashmap() {
-    for(auto road : Roads()) {
+    for(const Model::Road &road : Roads()) {
         if(road.type != Model::Road::Type::Footway) {
-            for(auto node_idx : Ways()[road.way].nodes) {
+            for(int node_idx : Ways()[road.way].nodes) {
                 if(node_to_road.find(node_idx) == node_to_road.end()){
-                    node_to_road[node_idx] = std::vector<const Model::Road*>{};
+                    node_to_road[node_idx] = std::vector<const Model::Road*>();
                 }
-                node_to_road[node_idx].push_back(road);
+                node_to_road[node_idx].push_back(&road);
             }
         }
     }
