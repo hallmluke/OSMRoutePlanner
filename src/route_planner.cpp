94dbe623ea9cf40d10db94e56bf215d034632ab2
Luke Hall <hallmluke@gmail.com>
2019-05-26 14:10:36 -0400

    Its not a point

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -7,8 +7,8 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
     end_x *= 0.01;
     end_y *= 0.01;
 
-    start_node = &m_Model.FindClosestNode(start_x, start_y);
-    end_node = &m_Model.FindClosestNode(end_x, end_y);
+    start_node = m_Model.FindClosestNode(start_x, start_y);
+    end_node = m_Model.FindClosestNode(end_x, end_y);
 }
 
 std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node * current_node) {
